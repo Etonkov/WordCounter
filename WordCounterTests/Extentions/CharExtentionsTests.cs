3451341faee9f0b5930dcdc0ef3d8cbc26eddcd6
Etonkov <evgeniy.tonkov@mail.ru>
2017-05-18 11:30:50 +0300

    Change folders

---
+++ WordCounterTests/Extentions/CharExtentionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Extentions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter.Extentions.Tests
+{
+    [TestClass()]
+    public class CharExtentionsTests
+    {
+        [TestMethod()]
+        public void IsWordPartTest()
+        {
+            // Arrange.
+            string wordChars = "1234567890qwertyuiopasdfghjklzxcvbnmйцукенгшщзхъфывапролджэячсмитьбю-_";
+            string notWordChars = " \t\n\r?.,[]{}'\";!@#$%^&*()/=+";
+
+            // Act + assert.
+            foreach (var charItem in wordChars.ToCharArray())
+            {
+                Assert.IsTrue(charItem.IsWordPart());
+            }
+
+            foreach (var charItem in notWordChars.ToCharArray())
+            {
+                Assert.IsFalse(charItem.IsWordPart());
+            }
+        }
+    }
+}
